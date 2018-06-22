ec74fa99c743629262358e5ac6caa757d8a191e6
Noitamin <genesis-ii@hotmail.com>
2018-06-22 18:25:27 +0000

    Removed unused commands

--- zooey.py
+++ zooey.py
@@ -66,26 +66,6 @@ async def on_message(message):
         await bot.process_commands(message)
         return
 
-@bot.command()
-async def hello():
-    """Says world"""
-    await bot.say("world")
-
-
-async def conjunction():
-    """Secret to winning MVP races"""
-    await bot.say("Enjoy your mvp!")
-
-@bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
-
-@bot.command()
-async def saber():
-    """rekt."""
-    await bot.say("git rekt")
-
 @bot.command(pass_context=True)
 async def big(ctx, message):
     """Hugify a given emoji"""
@@ -154,10 +134,6 @@ async def big(ctx, message):
 
     else:
         await bot.say("That's not a custom emoji. Try again")
-
-@bot.command()
-async def goat():
-    await bot.say("https://cdn.modernfarmer.com/wp-content/uploads/2013/09/saanen.jpg")
 
 @bot.command(pass_context=True)
 async def intense(ctx, *args):
